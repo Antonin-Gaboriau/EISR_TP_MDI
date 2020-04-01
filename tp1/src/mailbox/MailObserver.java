0ff934fd440694ceb996bea54c2216d79cc9752c
Nabs <60433869+ndresse@users.noreply.github.com>
2020-04-01 17:26:26 +0200

    test

--- tp1/src/mailbox/MailObserver.java
+++ tp1/src/mailbox/MailObserver.java
@@ -7,7 +7,6 @@ public class MailObserver extends JLabel implements Observer {
 	@Override
 	public void update(Subject mailbox) 
 	{
-		System.out.println("tamereenslip");
 		MailBox mailbox_cast = (MailBox) mailbox;
 		if(!mailbox_cast.isEmpty()) {this.setText(mailbox_cast.getLastMail().getSubject());}
 	}
